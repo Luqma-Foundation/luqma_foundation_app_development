80a0efd147a9c412bde7df68398e9bd3e40cd1b2
Fakhra15 <fakhramariyam@gmail.com>
2023-08-06 22:49:51 +0530

    modified files

--- LUQMA2/app/src/main/java/com/example/luqma/MainActivity.java
+++ LUQMA2/app/src/main/java/com/example/luqma/MainActivity.java
@@ -2,32 +2,34 @@ package com.example.luqma;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    Toolbar toolbars;
+    Toolbar toolbar;
 
 
-   // @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_1);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         drawerLayout = findViewById(R.id.layout);
         navigationView = findViewById(R.id.navigation_view);
+        toolbar = findViewById(R.id.toolbar);
 
-       // Toolbar toolbars = findViewById(R.id.toolbars);
-       // setSupportActionBar(toolbars);
+        setSupportActionBar(toolbar);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbars,R.string.navigation_open,R.string.navigation_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_open,R.string.navigation_close);
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
