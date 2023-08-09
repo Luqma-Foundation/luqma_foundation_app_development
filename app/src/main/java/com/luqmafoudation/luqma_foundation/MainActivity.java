5c7c898f4f7b7c288fa9f463ee5696a46541e105
Fakhra15 <fakhramariyam@gmail.com>
2023-08-09 14:23:48 +0530

    login_page

--- app/src/main/java/com/luqmafoudation/luqma_foundation/MainActivity.java
+++ app/src/main/java/com/luqmafoudation/luqma_foundation/MainActivity.java
@@ -1,12 +1,17 @@
 package com.luqmafoudation.luqma_foundation;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.navigation.NavigationView;
@@ -37,10 +42,45 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void ClickMenu(View view) {opeDrawer(drawerLayout);}
+
+    private void opeDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
     public void survey_page(View view) {
         Intent intent= new Intent(this,survey.class);
         startActivity(intent);
     }
+
+    public void logout(View view) {
+        logoutMenu(MainActivity.this);
+    }
+
+    private void logoutMenu(MainActivity mainActivity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to logout?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+
+
+
 
 
 
